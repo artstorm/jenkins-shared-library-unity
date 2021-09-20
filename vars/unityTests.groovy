a37c132158c2e149c68df17f983e9ded7e5c272f
Johan Steen <artstorm@gmail.com>
2021-09-20 08:50:11 +0700

    Pass in Unity version instead of path

--- vars/unityTests.groovy
+++ vars/unityTests.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String unityPath) {
-  sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/editmodetests.xml"
-  sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform PlayMode -testResults Reports/playmodetests.xml"
+def call(String unityVersion) {
+  String unityPath = "/Applications/Unity/Hub/Editor/${unityVersion}/Unity.app/Contents/MacOS/Unity"
+
+  sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/EditModeTests.xml"
+  sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform PlayMode -testResults Reports/PlayModeTests.xml"
 }
