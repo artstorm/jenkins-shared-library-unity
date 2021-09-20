59946a94cd45cad1abb769bcd0ccbf499fc13fc3
Johan Steen <artstorm@gmail.com>
2021-09-20 10:12:09 +0700

    Locks unity resource during testing

--- vars/unityTests.groovy
+++ vars/unityTests.groovy
@@ -3,6 +3,8 @@
 def call(String unityVersion) {
   String unityPath = "/Applications/Unity/Hub/Editor/${unityVersion}/Unity.app/Contents/MacOS/Unity"
 
-  sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/EditModeTests.xml"
-  sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform PlayMode -testResults Reports/PlayModeTests.xml"
+  lock('unity') {
+    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/EditModeTests.xml"
+    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform PlayMode -testResults Reports/PlayModeTests.xml"
+  }
 }
