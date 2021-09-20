a55ef504c714d2af880ddfa62bca5d9bdd8297f3
Johan Steen <artstorm@gmail.com>
2021-09-20 17:33:16 +0700

    Test multiline for sh

--- vars/unityTests.groovy
+++ vars/unityTests.groovy
@@ -4,7 +4,19 @@ def call(String unityVersion) {
   String unityPath = "/Applications/Unity/Hub/Editor/${unityVersion}/Unity.app/Contents/MacOS/Unity"
 
   lock('unity') {
-    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/EditModeTests.xml"
-    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform PlayMode -testResults Reports/PlayModeTests.xml"
+    sh """
+    ${unityPath}
+    -runTests
+    -batchmode
+    -buildTarget iOS
+    -testPlatform EditMode
+    -testResults Reports/EditModeTests.xml
+    -forgetProjectPath
+    -logFile log.txt
+    -projectPath .
+    """
+//    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/EditModeTests.xml"
+//    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform EditMode -testResults Reports/EditModeTests.xml"
+//    sh "${unityPath} -runTests -batchmode -buildTarget iOS -testPlatform PlayMode -testResults Reports/PlayModeTests.xml"
   }
 }
