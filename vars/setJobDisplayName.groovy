421501b4f7d6b0ff625f78c7bead5f7c9a6f135c
Johan Steen <artstorm@gmail.com>
2022-05-03 15:13:48 +0700

    Adds setJobDisplayName

---
+++ vars/setJobDisplayName.groovy
@@ -0,0 +1,22 @@
+/**
+ * Sets name for the running job based on git branch name.
+ *
+ * Currently I've no need to set the description.
+ */
+def call() {
+  switch(getBuildType()) {
+    case 'Internal':
+      currentBuild.displayName = "#${currentBuild.number} - Internal"
+      // currentBuild.description = "Internal build."
+      break
+
+    case 'TestFlight':
+      currentBuild.displayName = "#${currentBuild.number} - TestFlight"
+      // currentBuild.description = "TestFlight build."
+      break
+
+    default:
+      // Standard build, we don't change the name.
+      break
+  }
+}
