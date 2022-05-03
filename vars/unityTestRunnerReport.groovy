ee6cfb0f790cd217cd39ef7658b135646f7ff56f
Johan Steen <artstorm@gmail.com>
2022-05-03 15:07:23 +0700

    Adds report for test runner

---
+++ vars/unityTestRunnerReport.groovy
@@ -0,0 +1,15 @@
+/**
+ * Creates a merged report from Unitys PlayMode and EditMode test files.
+ *
+ * Uses the nunit report plugin.
+ * https://plugins.jenkins.io/nunit/
+ * https://www.jenkins.io/doc/pipeline/steps/nunit/
+ */
+def call() {
+  // NUnit report
+  nunit(
+    testResultsPattern: 'Reports/Tests/*.xml',
+    failIfNoResults: true,
+    healthScaleFactor: 1.0
+  )
+}
