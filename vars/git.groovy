4e64737145152b93d3052361fdd62bcc195c6192
Johan Steen <artstorm@gmail.com>
2022-05-16 09:37:49 +0700

    Adds initial git functions

---
+++ vars/git.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+
+/**
+ * Git functions.
+ *
+ * Multiple functions in one file:
+ * https://stackoverflow.com/q/69119132/1152087
+ */
+
+/**
+ * Get the full git commit sha for current commit.
+ */
+ def commitSha() {
+  return sh(returnStdout: true, script: "git rev-parse HEAD").trim();
+}
+
+/**
+ * Get the short 8 character git commit sha for current commit.
+ */
+def commitShaShort() {
+  return sh(returnStdout: true, script: "git rev-parse --short HEAD").trim();
+}
