de3bfe30ebba1fe1228118d7c11f25075fd596c7
Johan Steen <artstorm@gmail.com>
2022-05-16 10:34:18 +0700

    Adds branchName()

--- vars/git.groovy
+++ vars/git.groovy
@@ -8,6 +8,19 @@
  */
 
 /**
+ * Get the current branch name, with origin removed.
+ *
+ * `env.BRANCH_NAME` is set automatically in a multibranch pipeline,
+ * but not in regular pipeline, so we get it ourselves.
+ *
+ * Source:
+ * https://stackoverflow.com/q/39421257/1152087
+ */
+def branchName() {
+  return env.GIT_BRANCH.replaceFirst(/^.*?\//, '')
+}
+
+/**
  * Get the full git commit sha for current commit.
  */
  def commitSha() {
