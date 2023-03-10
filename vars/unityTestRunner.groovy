c47a254692f3709b2996d631f37dee49be94137c
Johan Steen <artstorm@gmail.com>
2023-03-10 21:21:39 +0700

    Add support for Unity testFilter option

--- vars/unityTestRunner.groovy
+++ vars/unityTestRunner.groovy
@@ -11,8 +11,9 @@
  * @param useCodeCoverage Determine if a code coverage report should be generated.
  * @param assemblyFilters Filters which assemblies to include/exclude in code coverage report.
  * @param pathFilters Filters which paths that should be included or excluded in the coverage report.
+ * @param testFilter Add a filter of which tests to run.
  */
-def call(String mode, String platform, Boolean useCodeCoverage = false, String assemblyFilters = '', String pathFilters = '') {
+def call(String mode, String platform, Boolean useCodeCoverage = false, String assemblyFilters = '', String pathFilters = '', String testFilter = '') {
   // Setup code coverage parameters to use with the testrunner, if `useCodeCoverage` is enabled.
   // https://docs.unity3d.com/Packages/com.unity.testtools.codecoverage@1.1/manual/CoverageBatchmode.html
   String codeCoverage = ''
@@ -38,6 +39,7 @@ def call(String mode, String platform, Boolean useCodeCoverage = false, String a
     -forgetProjectPath \
     -buildTarget ${platform} \
     -testPlatform ${mode} \
+    -testFilter "${testFilter}" \
     -testResults Reports/Tests/${platform}-${mode}.xml \
     -logFile Logs/tests-${platform}-${mode}.log \
     ${codeCoverage}
